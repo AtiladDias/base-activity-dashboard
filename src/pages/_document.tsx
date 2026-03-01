181f5e9da965ec5a5bc747674da3dfbc2b6af986
AtiladDias <128435253+AtiladDias@users.noreply.github.com>
2026-03-01 07:48:11 -0300

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="base:app_id" content="6952c55cc63ad876c9081871" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
