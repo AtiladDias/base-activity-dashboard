9b8d82a9d28ffb8c6f5e739b5624cf49ad93189d
AtiladDias <128435253+AtiladDias@users.noreply.github.com>
2026-03-17 13:21:55 -0300

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="talent-verification" content="<meta name="talentapp:project_verification" content="70b67f54ae35646b3cc7cc06cea2dc87343e079ce99a1f63231018c87179dcc3e60e326e21b62401714d887d060a43b99f7e3755687f6e82c0829445e1369008">" />
+    </>
+  );
+}
