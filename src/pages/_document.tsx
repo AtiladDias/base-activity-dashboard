49e4a65d64c11e313196c6ea8877ceed886f3533
AtiladDias <128435253+AtiladDias@users.noreply.github.com>
2026-03-18 13:16:45 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        <meta name="base:app_id" content="6952c55cc63ad876c9081871" />
+                <meta name="base:app_id" content="6952c55cc63ad876c9081871" />
+
+                <meta
+          name="talentapp:project_verification"
+          content="70b67f54ae35646b3cc7cc06cea2dc87343e079ce99a1f63231018c87179dcc3e60e326e21b62401714d887d060a43b99f7e3755687f6e82c0829445e1369008"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
